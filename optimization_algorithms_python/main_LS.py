b5323aee490e200297ef4d1864a7db6b4ee9826e
Nina Siaminou <nina.siaminou@gmail.com>
2021-05-25 02:27:38 +0300

    fixed: import the right problem class

--- optimization_algorithms_python/main_LS.py
+++ optimization_algorithms_python/main_LS.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from numpy import linalg as LA
 from numpy.linalg import matrix_rank
-from optim_algorithms import QuadraticUncon
+from optim_algorithms import LeastSquares
 from create_data import create_data_LS
 
 def main():
@@ -12,7 +12,7 @@ def main():
     A, b, x_init, f_val_init = create_data_LS(dims=(m,n))
 
     print("Solution via SGD")
-    fval_sgd, iter_sgd = QuadraticUncon.stochastic_gradient_descent(A, b, x_init, 0.1, f_val_init, criterion='gradient_norm', max_iter=100)
+    fval_sgd, iter_sgd = LeastSquares.stochastic_gradient_descent(A, b, x_init, 0.1, f_val_init, criterion='gradient_norm', max_iter=100)
 
    # Make the first plot
     plt.plot(fval_sgd[0:iter_sgd])
